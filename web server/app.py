c70a77a90a53ffa14efd777fdd4f1b0e22b846a5
umaryun <126294964+umaryun@users.noreply.github.com>
2024-07-11 05:56:25 +0100

    Update app.py

--- web server/app.py
+++ web server/app.py
@@ -1,30 +1,28 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request
 import json
 #import requests
 from urllib.request import urlopen
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():  
+
+@app.route('/api/hello')
+def index():
     address = request.environ['REMOTE_ADDR']
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
-        print (address)
+        address = address
     else:
         address = request.environ['HTTP_X_FORWARDED_FOR']
-        print (request.environ['HTTP_X_FORWARDED_FOR'])
     url = f'http://ipinfo.io/{address}?token=8684e95f280e95'
     response = urlopen(url)
     data = json.load(response)
-    ip = data['ip']
     city = data['city']
     api_key = "e4a20d5f0fdbc130656637e5ff74aa77"
     url3 = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
     response3 = urlopen(url3)
     data3 = json.load(response3)
-    print(data3['main']['temp'])
-    print()
     temp = data3['main']['temp'] - 273.15
     format_temp = f"{temp:.2f}"
-    name = request.args.get('name', 'mark')
-    return render_template('index.html', ip=address, city=city, temp=format_temp, name=name)
+    name = request.args.get('visitor_name', 'mark')
+    API_RESULT = {"client_ip": address, "location": city, "greeting": f"Hello, {name}!, the temperature is {format_temp} degrees celcius in {city}"}
+    return API_RESULT
